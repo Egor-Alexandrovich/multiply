30652a1dde0921d3edd4bc0bd9ce064aa35b63db
Egor <alexandrovich2572@gmail.com>
2019-09-28 17:26:00 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let firstArray = first.split('').reverse();
+  let secondArray = second.split('').reverse();
+  let outputArray = [];
+  for (let i = 0; i <= firstArray.length-1; i++) {
+      for (let j = 0; j<=secondArray.length-1; j++) {
+        multiplyFirstSecond = firstArray[i]*secondArray[j];
+        if (outputArray[i+j]) {
+          outputArray[i+j] =outputArray[i+j] + multiplyFirstSecond;
+        }
+        else {
+          outputArray[i+j] = multiplyFirstSecond;
+        }    
+      }
+  }
+  for (let i = 0; i <= outputArray.length-1; i++) {
+      let octatok = outputArray[i]%10;
+      let perenos = Math.floor(outputArray[i]/10);
+      if (perenos) {
+        if (outputArray[i+1]) {
+          outputArray[i+1] = outputArray[i+1] + perenos;
+          outputArray[i] = octatok;
+        }
+        else {
+          outputArray[i+1] = 0;
+          outputArray[i+1] = outputArray[i+1] + perenos;
+          outputArray[i] = octatok;
+
+        }
+      }
+  }
+  
+  return outputArray.reverse().join('');
 }
